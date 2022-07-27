a2c998437f7bff1bb30ad4012f5bc239ce9c5e38
sentyaanko <sentyaanko@gmail.com>
2022-07-27 22:24:11 +0900

    cleanup filename

--- src/tools/CreateHeadingIDs.ps1
+++ src/tools/CreateHeadingIDs.ps1
@@ -1,13 +1,13 @@
 ﻿# /docs/CodeRefs 以下の md ファイルから見出しを取得し、 HeadingID をファイルに出力する。
-# HeadingIDsForCodeRefs.md は /docs/CodeRefs 以下に置かれたファイルで使用するためのもの。
-# HeadingIDsForRoot.md は /docs に置かれたファイルで使用するためのもの。
+# HeadingIDsToCodeRefsForCodeRefs.md は /docs/CodeRefs 以下に置かれたファイルで使用するためのもの。
+# HeadingIDsToCodeRefsForRoot.md は /docs に置かれたファイルで使用するためのもの。
 function Set-SaAllHeadingIDs{
 	$Targets = @(
-		@{Filename ="HeadingIDsForCodeRefs.md"; AnchorPath="../.."},
-		@{Filename ="HeadingIDsForRoot.md"; AnchorPath="CodeRefs"}
+		@{Filename ="HeadingIDsToCodeRefsForCodeRefs.md"; AnchorPath="../.."},
+		@{Filename ="HeadingIDsToCodeRefsForRoot.md"; AnchorPath="CodeRefs"}
 	)
 
-	$MdFiles = Get-ChildItem -Recurse -Include *.md | Select-String '^#' | ForEach-Object {$LinkName=$_.Line -replace '^#+\s','';$RelativePath=$_.RelativePath($PWD) -replace '\\','/';$Anchor=($LinkName -replace '[:\(\)]','').ToLower();@{LinkName=$LinkName; RelativePath=$RelativePath; Anchor=$Anchor} }
+	$MdFiles = Get-ChildItem -Recurse -Path CodeRefs -Include *.md | Select-String '^#' | ForEach-Object {$LinkName=$_.Line -replace '^#+\s','';$RelativePath=$_.RelativePath("${PWD}\CodeRefs") -replace '\\','/';$Anchor=($LinkName -replace '[:\(\)]','').ToLower();@{LinkName=$LinkName; RelativePath=$RelativePath; Anchor=$Anchor} }
 
 	foreach($Target in $Targets.GetEnumerator()){
 		$myOut = (($MdFiles | ForEach-Object{"[$($_.LinkName)]: $($Target.AnchorPath)/$($_.RelativePath)#$($_.Anchor)"}) -join "`n") + "`n"
@@ -16,6 +16,6 @@ function Set-SaAllHeadingIDs{
 	}
 }
 
-Push-Location "$PSScriptRoot\..\..\docs\CodeRefs"
+Push-Location "$PSScriptRoot\..\..\docs"
 Set-SaAllHeadingIDs
 Pop-Location
